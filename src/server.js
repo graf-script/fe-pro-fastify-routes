4737abd4eaa256b5f12777ad60e38ed54a450956
Kirill Makhnovskiy <script.makhnovskiy@gmail.com>
2022-11-06 16:43:09 +0200

    try

--- src/server.js
+++ src/server.js
@@ -1,39 +1,6 @@
 import fastify from './index';
 
-server.post('/uppercase', (request, reply) => {
-  if (request.body.toLowerCase().includes('fuck')) {
-    reply.status(403).send('unresolved');
-  } else {
-    return request.body.toUpperCase();
-  }
-});
 
-server.post('/lowercase', (request, reply) => {
-  if (request.body.toLowerCase().includes('fuck')) {
-    reply.status(403).send('unresolved');
-  } else {
-    return request.body.toLowerCase();
-  }
-});
-
-server.get('/user/:id', (request, reply) => {
-  const {id = 0} = request.params;
-
-  if (id in users) {
-    reply.send(users[id]);
-  } else {
-    reply.status(404).send('User not exist');
-  }
-});
-
-server.get('/users', (request, reply) => {
-  const {filter, value} = request.query;
-  const result = Object.values(users).filter(({ value }) => {
-    return filter && value;
-  })
-
-  reply.send(result);
-});
 
 (async () => {
   try {
